6f27d493fd833b7b7e728d9890ed37fe96d02d37
mekkiamine <mekkiamine34@gmail.com>
2026-02-05 10:04:05 +0100

    first commit

---
+++ src/main/java/skylinkers/tn/mediconnectbackend/MediConnectBackendApplication.java
@@ -0,0 +1,13 @@
+package skylinkers.tn.mediconnectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediConnectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediConnectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skylinkers/tn/mediconnectbackend/MediConnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package skylinkers.tn.mediconnectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediConnectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
